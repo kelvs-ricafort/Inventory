e752a3105769cbf07f8e32648da40acc4458268d
Kelvin Campos Ricafort <60914571+kelvs-ricafort@users.noreply.github.com>
2026-03-21 18:20:37 +0800

    Update `ItemDao` and `ItemDetailsScreen` for better state handling.
    Key changes include:
    - Updated `ItemDao` to allow `getItem` to return a `Flow` of a nullable `Item`.
    - Added documentation and conflict strategy to `ItemDao.insert()`.
    - Updated the "Sell" button in `ItemDetailsScreen` to be disabled when the item is out of stock.

--- app/src/main/java/com/kelvsricafort101/wordpress/inventory/data/ItemDao.kt
+++ app/src/main/java/com/kelvsricafort101/wordpress/inventory/data/ItemDao.kt
@@ -8,8 +8,13 @@ import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Database access object to access the Inventory database
+ */
 @Dao
 interface ItemDao {
+    // Specify the conflict strategy as IGNORE, when the user tries to add an
+    // existing Item into the database Room ignores the conflict.
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(item: Item)
 
@@ -20,7 +25,7 @@ interface ItemDao {
     suspend fun delete(item: Item)
 
     @Query("SELECT * FROM items WHERE id = :id")
-    fun getItem(id: Int): Flow<Item>
+    fun getItem(id: Int): Flow<Item?>
 
     @Query("SELECT * FROM items ORDER BY name ASC")
     fun getAllItems(): Flow<List<Item>>
--- app/src/main/java/com/kelvsricafort101/wordpress/inventory/ui/item/ItemDetailsScreen.kt
+++ app/src/main/java/com/kelvsricafort101/wordpress/inventory/ui/item/ItemDetailsScreen.kt
@@ -124,7 +124,7 @@ private fun ItemDetailsBody(
             onClick = onSellItem,
             modifier = Modifier.fillMaxWidth(),
             shape = MaterialTheme.shapes.small,
-            enabled = true
+            enabled = !itemDetailsUiState.outOfStock
         ) {
             Text(
                 text = stringResource(R.string.sell)
