6f970eb0a83d15d73759d29d5afa0924dd143b3a
Kelvin Campos Ricafort <60914571+kelvs-ricafort@users.noreply.github.com>
2026-03-21 15:27:50 +0800

    Implement data loading in `HomeViewModel` using `ItemsRepository`.
    Key changes include:
    - Modified `HomeViewModel` to accept `ItemsRepository` as a constructor dependency.
    - Implemented `homeUiState` as a `StateFlow` by mapping the repository's item stream and using the `stateIn` operator.
    - Updated `AppViewModelProvider` to provide the `ItemsRepository` dependency from the application container to `HomeViewModel`.

--- app/src/main/java/com/kelvsricafort101/wordpress/inventory/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/kelvsricafort101/wordpress/inventory/ui/home/HomeViewModel.kt
@@ -1,12 +1,29 @@
 package com.kelvsricafort101.wordpress.inventory.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kelvsricafort101.wordpress.inventory.data.Item
+import com.kelvsricafort101.wordpress.inventory.data.ItemsRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 /**
  * ViewModel to retrieve all items in the Room database.
  */
-class HomeViewModel: ViewModel() {
+class HomeViewModel(itemsRepository: ItemsRepository): ViewModel() {
+    /**
+     * Holds home ui state. The list of items are retrieved from [ItemsRepository] and mapped to
+     * [HomeUiState]
+     */
+    val homeUiState: StateFlow<HomeUiState> =
+        itemsRepository.getAllItemsStream().map { HomeUiState(it) }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = HomeUiState()
+            )
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
