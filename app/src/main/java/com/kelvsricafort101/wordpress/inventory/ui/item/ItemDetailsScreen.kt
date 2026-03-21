2a14150938a15fffc98b09d630b56889bfe43464
Kelvin Campos Ricafort <60914571+kelvs-ricafort@users.noreply.github.com>
2026-03-21 16:59:01 +0800

    Integrate `ItemsRepository` into `ItemDetailsViewModel` to display item details.
    Key changes include:
    - Updated `ItemDetailsViewModel` to accept `ItemsRepository` and fetch item data as a `StateFlow`.
    - Modified `ItemDetailsScreen` to collect UI state from the `ItemDetailsViewModel`.
    - Updated `AppViewModelProvider` to inject the `itemsRepository` dependency into `ItemDetailsViewModel`.
    - Connected `ItemDetailsBody` to the reactive `uiState` to display dynamic item data.

--- app/src/main/java/com/kelvsricafort101/wordpress/inventory/ui/item/ItemDetailsScreen.kt
+++ app/src/main/java/com/kelvsricafort101/wordpress/inventory/ui/item/ItemDetailsScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -35,9 +36,11 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kelvsricafort101.wordpress.inventory.InventoryTopAppBar
 import com.kelvsricafort101.wordpress.inventory.R
 import com.kelvsricafort101.wordpress.inventory.data.Item
+import com.kelvsricafort101.wordpress.inventory.ui.AppViewModelProvider
 import com.kelvsricafort101.wordpress.inventory.ui.navigation.NavigationDestination
 import com.kelvsricafort101.wordpress.inventory.ui.theme.InventoryTheme
 
@@ -52,8 +55,10 @@ object ItemDetailsDestination : NavigationDestination {
 fun ItemDetailsScreen(
     navigateToEditItem: (Int) -> Unit,
     navigateBack: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: ItemDetailsViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ) {
+    val uiState = viewModel.uiState.collectAsState()
     Scaffold(
         topBar = {
             InventoryTopAppBar(
@@ -75,7 +80,7 @@ fun ItemDetailsScreen(
         }, modifier = modifier
     ) { innerPadding ->
         ItemDetailsBody(
-            itemDetailsUiState = ItemDetailsUiState(),
+            itemDetailsUiState = uiState.value,
             onSellItem = {  },
             onDelete = {  },
             modifier = Modifier
