71921708c772816372ecd8c46acbaf89cf260497
Kelvin Campos Ricafort <kelvs.r@outlook.com>
2026-09-18 15:44:46 +0800

    Update `compileSdk` and `targetSdk` to API level 37.
    Key changes include:
    - Updated `compileSdk` to version 37 and removed the `minorApiLevel` configuration.
    - Updated `targetSdk` to version 37.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,15 +7,13 @@ plugins {
 android {
     namespace = "com.kelvsricafort101.wordpress.inventory"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.kelvsricafort101.wordpress.inventory"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
