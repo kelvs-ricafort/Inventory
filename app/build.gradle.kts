6b47625de0d6b1a7a17d9b3992b9ee84afc75b39
Kelvin Campos Ricafort <kelvs.r@outlook.com>
2026-09-22 17:52:42 +0800

    Add Room database dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,11 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
+    // Room
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
 
     // Testing
     debugImplementation(libs.androidx.compose.ui.test.manifest)
